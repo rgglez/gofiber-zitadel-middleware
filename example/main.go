a382177d20a4fff1e10e69ac54682ff6d39a1c7e
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-11-04 16:14:33 -0600

    Fixed package name

--- example/main.go
+++ example/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	gofiberzitadel "github.com/rgglez/gofiber-zitadel-middleware/gofiberzitadel"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module gofiberzitadel
+module github.com/rgglez/gofiber-zitadel-middleware
 
 go 1.23.2
 
